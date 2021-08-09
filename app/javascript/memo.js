9a2c5b180a60463497e70e19c32759c15fd42e39
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-08-09 14:14:06 +0900

    jsでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,18 @@
+const buildHTML= function(XHR){
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-data">
+        投稿日時:${item.created_at}
+      </div>
+      <div class="post-item">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
+
 function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
@@ -8,6 +23,17 @@ function post (){
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      const html = buildHTML(XHR);
+      list.insertAdjacentHTML("afterend", html);
+      formText.value = "";
+    };
   });
 };
 
